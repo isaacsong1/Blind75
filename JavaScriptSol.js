e71a6646a2448935065282e8b3ce7feecf52a206
Isaac Song <isaacsongs10@gmail.com>
2024-01-23 13:21:12 -0800

    add my final soln

--- JavaScriptSol.js
+++ JavaScriptSol.js
@@ -107,4 +107,16 @@ var maxProfit = function(prices) {
     //     right ++;
     // }
     // return max_profit
+    // -----
+    // My final solution
+    let profit = 0;
+    let buyPrice = prices[0];
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < buyPrice) {
+            buyPrice = prices[i]
+        } else {
+            profit = Math.max(profit, prices[i] - buyPrice);
+        }
+    }
+    return profit;
 };
