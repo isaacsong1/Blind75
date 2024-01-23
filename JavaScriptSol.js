9865309c8f8a3e5b11a84f9894f1349870651e31
Isaac Song <isaacsongs10@gmail.com>
2024-01-23 13:33:48 -0800

    add directions

--- JavaScriptSol.js
+++ JavaScriptSol.js
@@ -127,20 +127,34 @@ var maxProfit = function(prices) {
 
 //? 217. Contains Duplicate (https://leetcode.com/problems/contains-duplicate/description/)
 /* DIRECTIONS
-
+    Given an integer array (nums), return true if any value appears at least twice in the array, and return false if every element is distinct
 */  
 
 /* EXAMPLES
-1:  
-
+1: 
+    Input: nums = [1, 2, 3, 1]
+    Output: true
 2:
-
+    Input: nums = [1, 2, 3, 4]
+    Output: false
+3:
+    Input: nums = [1, 1, 1, 3, 3, 4, 3, 2, 4, 2]
+    Output: true
 */
 
 /* CONSTRAINTS
-
+    1 <= nums.length <= 10^5
+    -10^9 <= nums[i] <= 10^9
 */
 
 /* IDEA
-
+    Create a set of this array and compare sizes. If same, return false, if not return true
 */
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+};
