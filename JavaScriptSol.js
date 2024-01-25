25ca59b05a36e7c3cd88c87e63a161a370afc360
Isaac Song <isaacsongs10@gmail.com>
2024-01-25 14:09:14 -0800

    add directions

--- JavaScriptSol.js
+++ JavaScriptSol.js
@@ -284,3 +284,32 @@ var productExceptSelf = function(nums) {
 /*---------------------------------------------------------------------------------------------------*/
 
 //? 53. Maximum Subarray (https://leetcode.com/problems/maximum-subarray/description/)
+/* DIRECTIONS
+    Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+    Subarray - Contiguous non-empty sequence of elements within an array
+*/  
+
+/* EXAMPLES
+1: 
+    Input: nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
+    Output: 6
+    Explanation: The subarray [4, -1, 2, 1] has the largest sum 6.
+2:
+    Input: nums = [1]
+    Output: 1
+    Explanation: The subarray [1] has the largest sum 1.
+3: 
+    Input: nums = [5, 4, -1, 7, 8]
+    Output: 6
+    Explanation: The subarray [5, 4, -1, 7, 8] has the largest sum 23.
+*/
+
+/* CONSTRAINTS
+    1 <= nums.length <= 10^5
+    -10^4 <= nums[i] <= 10^4
+*/
+
+/* IDEA
+    
+*/
