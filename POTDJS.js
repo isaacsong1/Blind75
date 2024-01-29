b024ebdd167bd4327ee598bb2d952169eee4dbd0
Isaac Song <isaacsongs10@gmail.com>
2024-01-29 15:17:05 -0800

    add potd file

---
+++ POTDJS.js
@@ -0,0 +1,31 @@
+//? 232. Implement Queue using Stacks (https://leetcode.com/problems/implement-queue-using-stacks/description/?envType=daily-question&envId=2024-01-29)
+/* DIRECTIONS
+    
+*/  
+
+/* EXAMPLES
+1: 
+    Input: 
+    Output: 
+    Explanation: 
+2:
+    Input: 
+    Output: 
+    Explanation: 
+*/
+
+/* CONSTRAINTS
+    
+*/
+
+/* IDEA
+    
+*/
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    
+};
