368e74588bfc41ddca58b5400d8238c9a59b417e
Isaac Song <isaacsongs10@gmail.com>
2024-01-30 16:05:35 -0800

    add notes

--- POTD.py
+++ POTD.py
@@ -80,6 +80,14 @@ class MyQueue:
     You are given an array of strings (tokens) that represents an arithmetic expression in Reverse Polish Notation (http://en.wikipedia.org/wiki/Reverse_Polish_notation)
 
     Evaluate the expression. Return an integer that represnets the value of the expression.
+
+    NOTE that:
+    - The valid operators are '+', '-', '*', and '/'.
+    - Each operand may be an integer or another expression.
+    - The division between two integeres always truncates TOWARDS ZERO.
+    - There will not be any division by zero.
+    - The input represents a valid arithmetic expression in a reverse polish notation.
+    - The answer and all the intermediate calculations can be represented in a 32-bit integer.
 '''
 
 ''' EXAMPLES
