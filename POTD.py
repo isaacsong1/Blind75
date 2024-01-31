bdf01cd7d2c3976848dec47154e9f423baaee225
Isaac Song <isaacsongs10@gmail.com>
2024-01-31 15:27:58 -0800

    add examples

--- POTD.py
+++ POTD.py
@@ -180,7 +180,15 @@ class Solution:
 '''
 
 ''' EXAMPLES
-
+1.
+    Input: temperatures = [73, 74, 75, 71, 69, 72, 76, 73]
+    Output: [1, 1, 4, 2, 1, 1, 0, 0]
+2.
+    Input: temperatures = [30, 40, 50, 60]
+    Output: [1, 1, 1, 0]
+3.
+    Input: temperatures = [30, 60, 90]
+    Output: [1, 1, 0]
 '''
 
 ''' CONSTRAINTS
