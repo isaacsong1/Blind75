cac7f4f0092465ea469d8642f028258a534dbe45
Isaac Song <isaacsongs10@gmail.com>
2024-02-01 16:38:33 -0800

    add examples

--- POTD.py
+++ POTD.py
@@ -236,13 +236,25 @@ class Solution:
 # 2/1/2024
 # 2966. Divide Array Into Arrays With Max Difference (https://leetcode.com/problems/divide-array-into-arrays-with-max-difference/description/?envType=daily-question&envId=2024-02-01)
 ''' DIRECTIONS
+    You are given an integer array, nums, of size n and a positive integer k.
 
+    Divide the array into one or more arrays of size 3 satisfying the following conditions:
+    - EACH element of nums should be in EXACTLY one array
+    - The difference between any two elements in one array is less than or equal to k.
+
+    RETURN a 2D array containing all the arrays. If it is impossible to satisfy the conditions, return an empty array. And if there are multiple answers, return any of them.
 '''
 
 ''' EXAMPLES
 1.
-    Input: 
-    Output:
+    Input: nums = [1, 3, 4, 8, 7, 9, 3, 5, 1], k = 2
+    Output: [[1, 1, 3], [3, 4, 5], [7, 8, 9]]
+    Explanation: We can divide the array into the following arrays: [1, 1, 3], [3, 4, 5] and [7, 8, 9].
+                The difference between any two elements in each array is less than or equal to 2.
+2.
+    Input: nums = [1, 3, 3, 2, 7, 3], k = 3
+    Output: []
+    Explanation: It is not possible to divide the array satisfying all the conditions.
 
 '''
 
