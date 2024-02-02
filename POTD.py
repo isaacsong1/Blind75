eb2ac491d34cef4ddd4068c19a8acd5cc60a1bf7
Isaac Song <isaacsongs10@gmail.com>
2024-02-01 16:45:44 -0800

    add constraints

--- POTD.py
+++ POTD.py
@@ -255,11 +255,14 @@ class Solution:
     Input: nums = [1, 3, 3, 2, 7, 3], k = 3
     Output: []
     Explanation: It is not possible to divide the array satisfying all the conditions.
-
 '''
 
 ''' CONSTRAINTS
-
+    n == nums.length
+    1 <= n <= 10^5
+    n is a multiple of 3.
+    1 <= nums[i] <= 10^5
+    1 <= k <= 10^5s
 '''
 
 ''' IDEA 
