17c983bb0237546cd0f8cae8ffb33c37a0bcd9de
Isaac Song <isaacsongs10@gmail.com>
2024-02-05 14:05:40 -0800

    add examples

--- POTD.py
+++ POTD.py
@@ -366,14 +366,19 @@ class Solution:
 # 2/2/2024
 # 387. First Unique Character in String (https://leetcode.com/problems/first-unique-character-in-a-string/description/?envType=daily-question&envId=2024-02-05)
 ''' DIRECTIONS
-    
+    Given a string, s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
 '''
 
 ''' EXAMPLES
 1.
-    Input: 
-    Output:
-
+    Input: s = "leetcode"
+    Output: 0
+1.
+    Input: s = "loveleetcode"
+    Output: 2
+1.
+    Input: s = "aabb"
+    Output: -1
 '''
 
 ''' CONSTRAINTS
