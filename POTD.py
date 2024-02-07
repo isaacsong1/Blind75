2e7811a0528fee2c3d22af86c0ba3e4bd3605edf
Isaac Song <isaacsongs10@gmail.com>
2024-02-07 15:00:31 -0800

    add examples

--- POTD.py
+++ POTD.py
@@ -481,9 +481,17 @@ class Solution:
 
 ''' EXAMPLES
 1.
-    Input: 
-    Output: 
-
+    Input: s = "tree"
+    Output: "eert"
+    Explanation: 'e' appears twice while 'r' and 't' both appear once. So 'e' must appear both before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+2.
+    Input: s = "cccaaa"
+    Output: "aaaccc"
+    Explanation: Both 'c' and 'a' appear three times, so both 'cccaaa' and 'aaaccc' are valid answers. Note that 'cacaca' is incorrect, as the same characters must be together.
+3.
+    Input: s = "Aabb"
+    Output: "bbAa"
+    Explanation: 'bbaA' is also a valid answer, but 'Aabb' is incorrect. Note that 'A' and 'a' are treated as two different characters.
 '''
 
 ''' CONSTRAINTS
