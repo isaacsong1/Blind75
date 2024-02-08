14c0a789bcd2a6fb7fa919ebdbebcfaa29476169
Isaac Song <isaacsongs10@gmail.com>
2024-02-08 14:38:57 -0800

    add directions

--- POTD.py
+++ POTD.py
@@ -539,7 +539,10 @@ class Solution:
 # 2/8/2024
 # 279. Perfect Squares (https://leetcode.com/problems/perfect-squares/description/?envType=daily-question&envId=2024-02-08)
 ''' DIRECTIONS
+    Given an integer n, return the least number of perfect square numbers that sum to n.
 
+    A perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself. For example, 1, 4, 9, 16 are perfect squares while
+    3 and 11 are not.
 '''
 
 ''' EXAMPLES
