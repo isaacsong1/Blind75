ca2c72d2de3e8698f033e75d065085fea6f75f73
Isaac Song <isaacsongs10@gmail.com>
2024-02-13 15:09:24 -0800

    add examples

--- POTD.py
+++ POTD.py
@@ -625,9 +625,17 @@ class Solution:
 
 ''' EXAMPLES
 1.
-    Input: 
-    Output: 
-    Explanation:
+    Input: words = ["abc", "car", "ada", "racecar", "cool"]
+    Output: "ada"
+    Explanation: The first string that is palindromic is "ada". Note that "racecar" is also palindromic, but it is not the first.
+2.
+    Input: words = ["notapalindrome", "racecar"]
+    Output: "racecar"
+    Explanation: The first ahnd only string that is palindromic is "racecar".
+3.
+    Input: words = ["def", "ghi"]
+    Output: ""
+    Explanation: There are no palindromic strings, so the empty string is returned
 '''
 
 ''' CONSTRAINTS
