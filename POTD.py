a5c1588e10c72843f2f7187ba777af7c80a6a21d
Isaac Song <isaacsongs10@gmail.com>
2024-02-13 15:07:11 -0800

    add directions

--- POTD.py
+++ POTD.py
@@ -618,7 +618,9 @@ class Solution:
 # 2/13/2024
 # 2108. Find First Palindromic String in the Array (https://leetcode.com/problems/find-first-palindromic-string-in-the-array/description/?envType=daily-question&envId=2024-02-13)
 ''' DIRECTIONS
-    
+    Given an array of strings, words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+
+    A string is palindromic if it reads the same forward and backward.
 '''
 
 ''' EXAMPLES
