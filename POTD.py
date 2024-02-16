f3a4db7446d23c01b6019260a4245b1d7acc2b14
Isaac Song <isaacsongs10@gmail.com>
2024-02-16 15:46:16 -0800

    add solution in js

--- POTD.py
+++ POTD.py
@@ -701,3 +701,30 @@ class Solution:
             our removal count k).
             - We increment the counter 'count' because even though the element v[i] remains partially, it still contributes to the count of unique integers that need to be removed.
 '''
+
+# /**
+#  * @param {number[]} arr
+#  * @param {number} k
+#  * @return {number}
+#  */
+# var findLeastNumOfUniqueInts = function(arr, k) {
+#     let mp = new Map();
+#     for (let a of arr) {
+#         mp.set(a, (mp.get(a) || 0) + 1);
+#     }
+
+#     let v = Array.from(mp.values());
+#     let count = 0;
+#     v.sort((a, b) => a - b);
+#     for (let i = 0; i < v.length; i++) {
+#         if (k > v[i]) {
+#             k -= v[i];
+#             v[i] = 0;
+#         } else {
+#             v[i] -= k;
+#             k = 0
+#         }
+#         if (v[i] !== 0) count++;
+#     }
+#     return count;
+# };
