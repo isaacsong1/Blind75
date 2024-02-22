3045fea4cd45759cb81d061c4b8e1c8a23518d4c
Isaac Song <isaacsongs10@gmail.com>
2024-02-22 14:47:24 -0800

    fix solution

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -88,17 +88,17 @@
 var isValid = function(s) {
     const stack = [];
     for (let c of s) {
-        if (c === '(' || '[' || '{' ) {
-            stack.append(c);
+        if (c === '(' || c === '[' || c === '{' ) {
+            stack.push(c);
         } else {
             if (!stack.length ||
-                (c === '(' && stack[stack.length - 1] !== ')') ||
-                (c === '{' && stack[stack.length - 1] !== '}') ||
-                (c === '[' && stack[stack.length - 1] !== ']')) {
+                (c === ')' && stack[stack.length - 1] !== '(') ||
+                (c === '}' && stack[stack.length - 1] !== '{') ||
+                (c === ']' && stack[stack.length - 1] !== '[')) {
                 return false;
             }
             stack.pop();
         }
     }
-    return true;
+    return !stack.length;
 };
