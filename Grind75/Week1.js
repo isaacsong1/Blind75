9f09c844d9a77ec010e17f7f0e6a63347f73b1f1
Isaac Song <isaacsongs10@gmail.com>
2024-02-22 13:59:20 -0800

    add grind75 folder with two sum

---
+++ Grind75/Week1.js
@@ -0,0 +1,47 @@
+//? 1. Two Sum (https://leetcode.com/problems/two-sum/)
+//! Completed during Software Engineering Bootcamp
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+    // Pseudocode
+    // Loop through the length of the nums array
+    // Nested loop through the same length of the nums array
+    // If index is the same, continue
+    // Else add the numbers at the indeces and see it they equal target
+
+    var twoSum = function(nums, target) {
+        //-----------------------------------------
+        // Time O(N) - linear
+        // Space O(N) - linear
+        // Initialize an empty hash map
+        const hash = Object.create(null);
+        // Iterate through the length of the nums array
+        for (let i = 0; i < nums.length; i++) {
+            // Initialize a pointer to the current value of nums
+            const currentEl = nums[i];
+            // Subtract the pointer to the target and see what value we are missing
+            const missingEl = target - currentEl;
+            // If there is a matching key for the missing element,
+            if (hash[missingEl] !== undefined) {
+                // Return an array of the index and what the missing value is
+                return [i, hash[missingEl]];
+            }
+            // Set the keys of the hash map to each value of nums and the entry to the index
+            hash[currentEl] = i
+    }
+    //-----------------------------------------
+    // Original Attempt
+    // Time O(N^2)
+    // Space O(1)
+    // for (let i = 0; i < nums.length; i++) { // At worst I loop through the whole array (N)
+    //     for (let j = i + 1; j < nums.length; j++) { // At worst I loop through the whole array (N)
+    //         if (nums[i] + nums[j] === target) {
+    //             return [i, j];
+    //         }
+    //     }
+    // }
+};
+
+/*---------------------------------------------------------------------------------------------------*/
