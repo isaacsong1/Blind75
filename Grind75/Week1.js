f0987583e8ce78b66b34d4448e48bc24630a8c36
Isaac Song <isaacsongs10@gmail.com>
2024-02-22 14:00:56 -0800

    add problem 20

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -45,3 +45,5 @@
 };
 
 /*---------------------------------------------------------------------------------------------------*/
+
+//? 20. Valid Parentheses (https://leetcode.com/problems/valid-parentheses/description/)
