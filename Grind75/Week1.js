c6fe5d67cfa9155f69d6913c52f1da4388c3b1c9
Isaac Song <isaacsongs10@gmail.com>
2024-02-22 14:04:15 -0800

    add examples

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -49,17 +49,24 @@
 //? 20. Valid Parentheses (https://leetcode.com/problems/valid-parentheses/description/)
 
 /* DIRECTIONS
-    
+    Given a string s containing just the characters '(', ')', '{', '}', '[', and ']', determine if the input string is valid.
+
+    An input string is valid if:
+    1. Open brackets must be closed by the same type of brackets.
+    2. Open brackets must be closed in the correct order.
+    3. Every close bracket has a corresponding open bracket of the same type.
 */  
 
 /* EXAMPLES
 1:  
-    Input: 
-    Output: 
-    Explanation: 
-
+    Input: s = '()'
+    Output: true
 2:
-    
+    Input: s = '()[]{}'
+    Output: true
+3:
+    Input: s = '(]'
+    Output: false
 */
 
 /* CONSTRAINTS
