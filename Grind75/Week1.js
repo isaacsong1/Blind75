4634c18cfea1222e69bec24d9f5a62f4f92c517a
Isaac Song <isaacsongs10@gmail.com>
2024-02-23 13:02:42 -0800

    add directions

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -111,7 +111,11 @@ var isValid = function(s) {
 //! Done during bootcamp
 
 /* DIRECTIONS
+    You are given the heads of two sorted linked lists list1 and list2.
+
+    Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
     
+    Return the head of the merged linked list.
 */  
 
 /* EXAMPLES
