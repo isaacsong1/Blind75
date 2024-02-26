3699746364522100490e6916e11d523f7b3f27a6
Isaac Song <isaacsongs10@gmail.com>
2024-02-26 13:35:36 -0800

    add directions

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -274,6 +274,14 @@ var maxProfit = function(prices) {
 // T - Test
 // O - Optimization
 
+/* DIRECTIONS
+    A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward.
+    Alphanumeric characters include letters and numbers.
+
+    Given a string s, return true if it is a palindrome or false otherwise.
+*/  
+
+
 
 /* CONSTRAINTS
     
