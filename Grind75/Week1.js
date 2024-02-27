376bfd8fa87cd519889487070b375ad083433829
Isaac Song <isaacsongs10@gmail.com>
2024-02-27 15:16:24 -0800

    add examples

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -328,7 +328,15 @@ var maxProfit = function(prices) {
 */  
 
 /* EXAMPLES
-
+1:  
+    Input: root = 4, 2, 7, 1, 3, 6, 9]
+    Output: [4, 7, 2, 9, 6, 3, 1]
+2:
+    Input: root = [2, 1, 3]
+    Output: [2, 3, 1]
+3:
+    Input: root = []
+    Output: []
 */
 
 /* CONSTRAINTS
