73cda8aa8dd7488ce4d143d6876f864e84bc37e9
Isaac Song <isaacsongs10@gmail.com>
2024-02-29 12:27:12 -0800

    add directions

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -374,7 +374,9 @@ var maxProfit = function(prices) {
 //? 242. Valid Anagram (https://leetcode.com/problems/valid-anagram/description/)
 
 /* DIRECTIONS
-    
+    Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+    An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 */  
 
 /* EXAMPLES
