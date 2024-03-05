4aacb613757987f016649591a35ce6febc8c565a
Isaac Song <isaacsongs10@gmail.com>
2024-03-05 15:21:52 -0800

    add code solution

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -523,3 +523,31 @@ var search = function(nums, target) {
 
     NOTE: I followed a youtube video since solutions were a bit confusing (https://www.youtube.com/watch?v=OODFEqJxiDo)
 */
+
+/**
+ * @param {number[][]} image
+ * @param {number} sr
+ * @param {number} sc
+ * @param {number} color
+ * @return {number[][]}
+ */
+var floodFill = function(image, sr, sc, color) {
+    const startingPoint = image[sr][sc];
+
+    function recurse(image, sr, sc) {
+        // Check boundaries
+        if (sr < 0 || sr > image.length - 1 || sc < 0 || sc > image[0].length - 1 || image[sr][sc] !== startingPoint || 
+        image[sr][sc] === color) return image;
+
+        image[sr][sc] = color;
+
+        recurse(image, sr + 1, sc);
+        recurse(image, sr - 1, sc);
+        recurse(image, sr, sc + 1);
+        recurse(image, sr, sc - 1);
+
+        return image;
+    }
+
+    return recurse(image, sr, sc);
+};
