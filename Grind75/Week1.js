7d67942d00cd3ada17a55729edb61e2128b51603
Isaac Song <isaacsongs10@gmail.com>
2024-03-05 12:54:41 -0800

    add exampels

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -497,11 +497,14 @@ var search = function(nums, target) {
 
 /* EXAMPLES
 1:  
-    Input: 
-    Output: 
+    Input: image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]], sr = 1, sc = 1, color = 2
+    Output: [[2, 2, 2], [2, 2, 0], [2, 0, 1]]
+    Explanation: From the center of the image with position (sr, sc) = (1, 1), all pixels connected by a path of the same color as the starting pixel are colored with the new color.
+        Note that the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
 2:
-    Input: 
-    Output: 
+    Input: image = [[0, 0, 0], [0, 0, 0]], sr = 0, sc = 0, color = 0
+    Output: [[0, 0, 0], [0, 0, 0]]
+    Explanation: The starting pixel is already colored 0, so no changes are made to the image.
 */
 
 /* CONSTRAINTS
