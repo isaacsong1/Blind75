a8abc88668fc9188d51d378caa4f59417de904ee
Isaac Song <isaacsongs10@gmail.com>
2024-03-09 14:54:36 -0800

    add directions

--- Practice/practice.js
+++ Practice/practice.js
@@ -1,7 +1,7 @@
 //? 938. Range Sum of BST (https://leetcode.com/problems/range-sum-of-bst/description/)
 
 /* DIRECTIONS
-    
+    Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
 */  
 
 /* EXAMPLES
