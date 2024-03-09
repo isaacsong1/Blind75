3aebcbdf99bf77aea6a469fb2aff0e94b5598bba
Isaac Song <isaacsongs10@gmail.com>
2024-03-09 14:54:02 -0800

    add easy bst problem 938

---
+++ Practice/practice.js
@@ -0,0 +1,24 @@
+//? 938. Range Sum of BST (https://leetcode.com/problems/range-sum-of-bst/description/)
+
+/* DIRECTIONS
+    
+*/  
+
+/* EXAMPLES
+1:  
+    Input: 
+    Output: 
+    Explanation: 
+2:
+    Input: 
+    Output: 
+    Explanation: 
+*/
+
+/* CONSTRAINTS
+    
+*/
+
+/* IDEA
+    
+*/
