c54c060d40fee7aead02a0e4316c9ebc21967888
Isaac Song <isaacsongs10@gmail.com>
2024-05-23 14:53:10 -0700

    add directions and examples

--- Grind75/Week1.js
+++ Grind75/Week1.js
@@ -570,19 +570,22 @@ var floodFill = function(image, sr, sc, color) {
 /* DIRECTIONS
     Given a binary search tree (BST), find the lowest common ancestor (LCA) node of two given nodes in the BST.
 
-    According to the definition of LCA on Wikipedia (https://en.wikipedia.org/wiki/Lowest_common_ancestor): 'The lowest common ancestor is defined between two nodes p and q as the lowest
-    node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).'
+    According to the definition of LCA on Wikipedia (https://en.wikipedia.org/wiki/Lowest_common_ancestor): 'The lowest common ancestor is defined between two nodes, p and q, as the
+    lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).'
 */  
 
 /* EXAMPLES
 1:  
-    Input: 
-    Output: 
-    Explanation: 
+    Input: root = [6, 2, 8, 0, 4, 7, 9, null, null, 3, 5], p = 2, q = 8
+    Output: 6
+    Explanation: The LCA of nodes 2 and 8 is 6
 2:
-    Input: 
-    Output: 
-    Explanation: 
+    Input: root = [6, 2, 8, 0, 4, 7, 9, null, null, 3, 5], p = 2, q = 4
+    Output: 2
+    Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself according to the LCA definition
+3:
+    Input: root = [2, 1], p = 2, q = 1
+    Output: 2
 */
 
 /* CONSTRAINTS
